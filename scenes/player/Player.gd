e39fe7c3fc4aeebc743e4c91f78f40b117a58bb6
Eliana Barrios <elianabarrios9827@gmail.com>
2022-05-31 16:05:16 -0300

    Player Movement

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,1 +1,14 @@
 extends KinematicBody2D
+
+const SPEED = 300
+var velocity = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
+		velocity.x = 0
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	move_and_slide(velocity)
+pass
